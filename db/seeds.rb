7391400a060224ee205dfc37e8bf5357a50d87a7
bl2i4n <bl2i4n@yahoo.com>
2017-06-11 02:04:22 -0700

    installed faker and created user_data file for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+include Faker
+require 'user_data'
+
+10.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+users = User.all
+
+10.times do
+  Item.create!(
+  name: Faker::Lorem.sentence,
+  )
+end
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} todos created"
