2e24e16167dd42f65c4a1962cb9689583fa190fa
bl2i4n <bl2i4n@yahoo.com>
2017-06-10 20:25:23 -0700

    finished todo functionality, need to fix home button and navbar

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   #need create todo
   #need edit todo
   #need delete todo
+
+  def create
+    @user = User.find(params[:user_id])
+    @item = current_user.items.create(item_params)
+    redirect_to root_path
+  end
+
+  def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-
-  get 'welcome/index'
-
   devise_for :users
-  resources :users, only: [:show]
-  resources :item
+  resources :users, only: [:show] do
+    resources :items, only: [:create, :destroy]
+  end
 
   authenticated :user do
     root 'users#show', as: :authenticated_root
